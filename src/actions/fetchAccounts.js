c20a53e5e76d891d54e3acb6d2529944e8692730
Huda Yousif <hjoski@gmail.com>
2020-07-08 01:52:44 -0700

    set up account reducer and fetchaccounts

---
+++ src/actions/fetchAccounts.js
@@ -0,0 +1,9 @@
+export function fetchAccounts(action) {
+
+  // fetch('http://localhost:3000/api/v1/accounts')
+  // .then(resp => resp.json())
+  // .then(data => console.log(data))
+
+  return action
+
+  }
---
+++ src/reducers/accountReducer.js
@@ -0,0 +1,5 @@
+export default function accountReducer(state = {accounts: []}, action) {
+
+    return action.payload
+// make sure action was coming in correctly and be updated by reducer
+}
