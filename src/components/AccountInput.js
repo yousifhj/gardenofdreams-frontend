01b7607b1f22535236bd43511780c230bad1939d
Huda Yousif <hjoski@gmail.com>
2020-07-09 01:36:48 -0700

    create accounts accountinput and accountcontainer

---
+++ src/components/AccountInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class AccountInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                AccountInput
+            </div>
+        )
+
+    }
+}
+
+export default AccountInput
