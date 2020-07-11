be9f47dc18e4c84b67c1a0f626bc28afc8835ad1
Huda Yousif <hjoski@gmail.com>
2020-07-11 00:44:48 -0700

    create form

--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -1,15 +1,42 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addAccount} from '../actions/addAccount'
 
 class AccountInput extends React.Component {
 
+    state = {
+        name: '',
+        balance: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addAccount(this.state)
+        this.setState({
+          name: '',
+          balance: ''
+        })
+    }
+
     render() {
         return (
-            <div>
-                AccountInput
-            </div>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <label>Garden of Dreams Name: </label>
+              <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+              <label>Garden of Dreams Balance: </label>
+              <input type='text' placeholder='Balance' value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
+              <input type="submit"/>
+            </form>
+          </div>
         )
-
+      }
     }
-}
 
-export default AccountInput
+    export default connect(null, {addAccount})(AccountInput)
