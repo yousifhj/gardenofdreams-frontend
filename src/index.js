3c5fc831bb1a57dcef88b199d012a0bdd9d23767
Huda Yousif <hjoski@gmail.com>
2020-07-12 23:32:15 -0700

    add router to index file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import accountReducer from './reducers/accountReducer'
 
 import App from './App';
@@ -14,6 +15,8 @@ let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
       <App />
+    </Router>
   </Provider>,
   document.getElementById('root'));
