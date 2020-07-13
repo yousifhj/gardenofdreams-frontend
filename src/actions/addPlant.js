88c553cf4368dc7a69736af9abbb2faff3ba3916
Huda Yousif <hjoski@gmail.com>
2020-07-13 02:45:03 -0700

    add in plant fetch and  plantinput

---
+++ src/actions/addPlant.js
@@ -0,0 +1,14 @@
+export const addPlant = (plant, accountId) => {
+
+    return (dispatch) => {
+      fetch(`http://localhost:3000/api/v1/accounts/${accountId}/plants`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(plant)
+      })
+      .then(response => response.json())
+      .then(account => dispatch({type: 'ADD_PLANT', payload: account}))
+    }
+}
--- src/components/PlantInput.js
+++ src/components/PlantInput.js
@@ -1,15 +1,54 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addPlant} from '../actions/addPlant'
 
 class PlantInput extends React.Component {
 
+    state = { 
+        kinda: 'gifted',
+        name: '',
+        price: '',
+        description: '',
+    
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addPlant(this.state, this.props.account.id)
+        this.setState({
+            kinda: 'gifted',
+            name: '',
+            price: '',
+            description: '',
+        })
+    }
     render() {
         return (
             <div>
-                PlantInput
+                <form onSubmit={this.handleSubmit}>
+                    <label> Plant Type:</label>
+                    <select name="kinda" value={this.state.kind} onChange={this.handleChange}>
+                        <option>gift</option>
+                        <option>gifted</option>
+                    </select>
+                    <label>Plant Name:</label>
+                    <input type="text" name="name"value={this.state.name} onChange={this.handleChange}></input>
+                    <label>Plant Price:</label>
+                    <input type="text" name="price"value={this.state.price} onChange={this.handleChange}></input>
+                    <label>Plant Description:</label>
+                    <textarea type="text" name="description" value={this.state.description} onChange={this.handleChange}></textarea>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
 
 }
 
-export default PlantInput 
+export default connect(null, {addPlant}) (PlantInput)
