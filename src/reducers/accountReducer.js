5e29ac6f9c80beed8c1b92754444e2a66180384c
Huda Yousif <hjoski@gmail.com>
2020-07-14 01:19:10 -0700

    update reducer

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,13 +1,49 @@
+
+const initState = {
+  accounts: []
+}
+
+export default (state = initState, action) => {
+  const { type, payload } = action
+  if (type === 'FETCH_ACCOUNTS') {
+
+    return {
+      ...state,
+      accounts: payload
+    }
+  }
+  if (type === 'ADD_ACCOUNT') {
+    return {
+      ...state,
+      accounts: [...state.accounts, payload]
+    }
+  }
+  if (type === 'ADD_PLANT') {
+    console.log('updated plant', payload)
+    return {
+      ...state,
+      accounts: [...state.accounts, payload]
+    }
+  }
+
+  return state
+}
+/*
 export default function accountReducer(state = {accounts: []}, action) {
-  debugger
+  
     switch (action.type) {
         case 'FETCH_ACCOUNTS':
-          return {accounts: action.payload}
+        
+          return {...state, accounts: action.payload}
           case 'ADD_ACCOUNT':
+            
             return {...state, accounts: [...state.accounts, action.payload]}
           case 'ADD_PLANT':
-              return {}
+              return {
+                ...state,
+                accounts: [...state.accoutn]
+              }
         default:
           return state
     }
-}
+}*/
