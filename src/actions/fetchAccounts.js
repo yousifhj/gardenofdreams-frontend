75f8e2787baeb4d3bebd80920941ba3b38dbecfa
Huda Yousif <hjoski@gmail.com>
2020-07-22 22:18:05 -0700

    update fetchaccount

--- src/actions/fetchAccounts.js
+++ src/actions/fetchAccounts.js
@@ -8,3 +8,14 @@ export function fetchAccounts() {
     }))
   }
 }
+
+export function fetchAccount (id) {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/accounts/${id}`)
+    .then(resp => resp.json())
+    .then(account => dispatch({
+      type: 'FETCH_ACCOUNT',
+      payload: account
+    }))
+  }
+}
